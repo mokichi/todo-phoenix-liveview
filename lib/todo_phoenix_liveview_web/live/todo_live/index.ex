a61baebc3cadb20ea9fbc2845d1868a5c4fe3c08
SAITO Kazuya <saito@smartalgorithm.co.jp>
2020-12-26 11:37:54 +0900

    Create task

--- lib/todo_phoenix_liveview_web/live/todo_live/index.ex
+++ lib/todo_phoenix_liveview_web/live/todo_live/index.ex
@@ -10,6 +10,12 @@ defmodule TodoPhoenixLiveviewWeb.TodoLive.Index do
   end
 
   @impl true
+  def handle_event("create_task", %{"task" => %{"title" => _} = task_params}, socket) do
+    {:ok, _} = Todo.create_task(task_params)
+    {:noreply, assign_params(socket)}
+  end
+
+  @impl true
   def handle_event("toggle_completed", %{"id" => id, "completed" => completed}, socket) do
     task = Todo.get_task!(id)
     {:ok, _} = Todo.update_task(task, %{completed: completed})
@@ -19,6 +25,7 @@ defmodule TodoPhoenixLiveviewWeb.TodoLive.Index do
 
   defp assign_params(socket) do
     socket
+    |> assign(:changeset, Todo.change_task(%Task{}))
     |> assign(:tasks, Todo.list_tasks_by_completed(false))
     |> assign(:completed_tasks, Todo.list_tasks_by_completed(true))
   end
