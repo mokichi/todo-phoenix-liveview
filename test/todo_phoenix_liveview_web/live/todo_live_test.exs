5b487878a78886f6bd443212bce68953528dc606
SAITO Kazuya <saito@smartalgorithm.co.jp>
2021-01-16 16:14:07 +0900

    Add TodoLive tests

---
+++ test/todo_phoenix_liveview_web/live/todo_live_test.exs
@@ -0,0 +1,104 @@
+defmodule TodoPhoenixLiveview.TodoLiveTest do
+  use TodoPhoenixLiveviewWeb.ConnCase
+
+  import Phoenix.LiveViewTest
+
+  alias TodoPhoenixLiveview.Todo
+
+  defp create_task(_) do
+    {:ok, task} = Todo.create_task(%{title: "Task 1", completed: false})
+    %{task: task}
+  end
+
+  defp create_completed_task(_) do
+    {:ok, task} = Todo.create_task(%{title: "Task 2", completed: true})
+    %{completed_task: task}
+  end
+
+  describe "Index" do
+    setup [:create_task, :create_completed_task]
+
+    test "lists all tasks", %{conn: conn, task: task, completed_task: completed_task} do
+      {:ok, view, _html} = live(conn, Routes.todo_index_path(conn, :index))
+
+      assert view |> has_element?("ul:nth-of-type(1) li", task.title)
+      assert view |> has_element?("ul:nth-of-type(2) li", completed_task.title)
+    end
+
+    test "creates new task", %{conn: conn} do
+      {:ok, view, _html} = live(conn, Routes.todo_index_path(conn, :index))
+
+      new_task_title = "New task"
+      view
+      |> form("form", %{task: %{title: new_task_title}})
+      |> render_submit()
+
+      assert Todo.list_tasks_by_completed(false)
+             |> Enum.at(1)
+             |> Map.get(:title) == new_task_title
+
+      assert view |> has_element?("ul:nth-of-type(1) li", new_task_title)
+    end
+
+    test "completes incompleted task", %{conn: conn, task: task} do
+      {:ok, view, _html} = live(conn, Routes.todo_index_path(conn, :index))
+
+      view
+      |> element("input[type=checkbox][phx-value-id=#{task.id}")
+      |> render_click()
+
+      assert Todo.list_tasks_by_completed(true)
+             |> Enum.at(1)
+             |> Map.get(:title) == task.title
+
+      refute view |> has_element?("ul:nth-of-type(1) li", task.title)
+      assert view |> has_element?("ul:nth-of-type(2) li", task.title)
+    end
+
+    test "incompletes completed task", %{conn: conn, completed_task: task} do
+      {:ok, view, _html} = live(conn, Routes.todo_index_path(conn, :index))
+
+      view
+      |> element("input[type=checkbox][phx-value-id=#{task.id}")
+      |> render_click()
+
+      assert Todo.list_tasks_by_completed(false)
+             |> Enum.at(1)
+             |> Map.get(:title) == task.title
+
+      assert view |> has_element?("ul:nth-of-type(1) li", task.title)
+      refute view |> has_element?("ul:nth-of-type(2) li", task.title)
+    end
+
+    test "updates task", %{conn: conn} do
+      {:ok, view, _html} = live(conn, Routes.todo_index_path(conn, :index))
+
+      view
+      |> element("ul:nth-of-type(1) li button", "Edit")
+      |> render_click()
+
+      new_task_title = "Updated task"
+      view
+      |> form("ul:nth-of-type(1) li form", %{task: %{title: new_task_title}})
+      |> render_submit()
+
+      assert Todo.list_tasks_by_completed(false)
+             |> Enum.at(0)
+             |> Map.get(:title) == new_task_title
+
+      assert view |> has_element?("ul:nth-of-type(1) li button", "Edit")
+    end
+
+    test "deletes task", %{conn: conn, task: task} do
+      {:ok, view, _html} = live(conn, Routes.todo_index_path(conn, :index))
+
+      view
+      |> element("button[phx-value-id=#{task.id}]", "Delete")
+      |> render_click()
+
+      assert_raise Ecto.NoResultsError, fn -> Todo.get_task!(task.id) end
+
+      refute view |> has_element?("ul li", task.title)
+    end
+  end
+end
