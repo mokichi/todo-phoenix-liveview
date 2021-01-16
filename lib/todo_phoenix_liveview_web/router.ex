17525cbe713eed02f2971fb8b94888ae0f646931
SAITO Kazuya <saito@smartalgorithm.co.jp>
2021-01-16 14:25:37 +0900

    Remove unused files

--- lib/todo_phoenix_liveview_web/router.ex
+++ lib/todo_phoenix_liveview_web/router.ex
@@ -17,14 +17,7 @@ defmodule TodoPhoenixLiveviewWeb.Router do
   scope "/", TodoPhoenixLiveviewWeb do
     pipe_through :browser
 
-    # live "/", PageLive, :index
     live "/", TodoLive.Index, :index
-
-    live "/tasks", TaskLive.Index, :index
-    live "/tasks/new", TaskLive.Index, :new
-    live "/tasks/:id/edit", TaskLive.Index, :edit
-    live "/tasks/:id", TaskLive.Show, :show
-    live "/tasks/:id/show/edit", TaskLive.Show, :edit
   end
 
   # Other scopes may use custom stacks.
