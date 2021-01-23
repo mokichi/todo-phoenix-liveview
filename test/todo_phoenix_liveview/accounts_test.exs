5f8c460ceada78babaff6e4bf9ed4e819958dd3b
SAITO Kazuya <saito@smartalgorithm.co.jp>
2021-01-23 11:06:28 +0900

    Fix tests

--- test/todo_phoenix_liveview/accounts_test.exs
+++ test/todo_phoenix_liveview/accounts_test.exs
@@ -21,7 +21,7 @@ defmodule TodoPhoenixLiveview.AccountsTest do
       refute Accounts.get_user_by_email_and_password("unknown@example.com", "hello world!")
     end
 
-    test "does not return the user if the password is not valid" do
+    test "does not return the user if the password is invalid" do
       user = user_fixture()
       refute Accounts.get_user_by_email_and_password(user.email, "invalid")
     end
@@ -58,11 +58,11 @@ defmodule TodoPhoenixLiveview.AccountsTest do
     end
 
     test "validates email and password when given" do
-      {:error, changeset} = Accounts.register_user(%{email: "not valid", password: "not valid"})
+      {:error, changeset} = Accounts.register_user(%{email: "invalid", password: "invalid"})
 
       assert %{
                email: ["must have the @ sign and no spaces"],
-               password: ["should be at least 12 character(s)"]
+               password: ["should be at least 8 character(s)"]
              } = errors_on(changeset)
     end
 
@@ -132,7 +132,7 @@ defmodule TodoPhoenixLiveview.AccountsTest do
 
     test "validates email", %{user: user} do
       {:error, changeset} =
-        Accounts.apply_user_email(user, valid_user_password(), %{email: "not valid"})
+        Accounts.apply_user_email(user, valid_user_password(), %{email: "invalid"})
 
       assert %{email: ["must have the @ sign and no spaces"]} = errors_on(changeset)
     end
@@ -258,12 +258,12 @@ defmodule TodoPhoenixLiveview.AccountsTest do
     test "validates password", %{user: user} do
       {:error, changeset} =
         Accounts.update_user_password(user, valid_user_password(), %{
-          password: "not valid",
+          password: "invalid",
           password_confirmation: "another"
         })
 
       assert %{
-               password: ["should be at least 12 character(s)"],
+               password: ["should be at least 8 character(s)"],
                password_confirmation: ["does not match password"]
              } = errors_on(changeset)
     end
@@ -467,12 +467,12 @@ defmodule TodoPhoenixLiveview.AccountsTest do
     test "validates password", %{user: user} do
       {:error, changeset} =
         Accounts.reset_user_password(user, %{
-          password: "not valid",
+          password: "invalid",
           password_confirmation: "another"
         })
 
       assert %{
-               password: ["should be at least 12 character(s)"],
+               password: ["should be at least 8 character(s)"],
                password_confirmation: ["does not match password"]
              } = errors_on(changeset)
     end
--- test/todo_phoenix_liveview_web/live/todo_live_test.exs
+++ test/todo_phoenix_liveview_web/live/todo_live_test.exs
@@ -5,18 +5,18 @@ defmodule TodoPhoenixLiveview.TodoLiveTest do
 
   alias TodoPhoenixLiveview.Todo
 
-  defp create_task(_) do
-    {:ok, task} = Todo.create_task(%{title: "Task 1", completed: false})
+  defp create_task(%{user: user}) do
+    {:ok, task} = Todo.create_task(%{title: "Task 1", completed: false, user_id: user.id})
     %{task: task}
   end
 
-  defp create_completed_task(_) do
-    {:ok, task} = Todo.create_task(%{title: "Task 2", completed: true})
+  defp create_completed_task(%{user: user}) do
+    {:ok, task} = Todo.create_task(%{title: "Task 2", completed: true, user_id: user.id})
     %{completed_task: task}
   end
 
   describe "Index" do
-    setup [:create_task, :create_completed_task]
+    setup [:register_and_log_in_user, :create_task, :create_completed_task]
 
     test "lists all tasks", %{conn: conn, task: task, completed_task: completed_task} do
       {:ok, view, _html} = live(conn, Routes.todo_index_path(conn, :index))
